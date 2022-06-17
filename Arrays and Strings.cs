2b1f0e9a0d7f7b5d2ee57d1a8c0686624fb16829
Bart <trenixdecease@live.com>
2022-06-17 13:36:42 -0400

    Create Arrays and Strings.cs

---
+++ Arrays and Strings.cs
@@ -0,0 +1,51 @@
+/*
+The program you are given defines an array with 10 words and takes a letter as input.
+Write a program to iterate through the array and output words containing the taken letter.
+If there is no such word, the program should output "No match". 
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Code_Coach_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = {
+                "home",
+                "programming",
+                "victory",
+                "C#",
+                "football",
+                "sport",
+                "book",
+                "learn",
+                "dream",
+                "fun"
+            };
+
+            string letter = Console.ReadLine();
+
+            int count = 0;
+            
+            //your code goes here
+            foreach (string x in words)
+            {
+                if (x.Contains(letter))
+                {
+                    count += 1;
+                    Console.WriteLine(x);
+                }
+
+
+            }
+
+            if (count == 0)
+                {
+                    Console.WriteLine("No match");
+                }
+        }
+    }
+}
