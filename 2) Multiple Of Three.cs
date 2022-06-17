61d85bf0b534be1b942649ebb6dc0435aac0cb05
Bart <trenixdecease@live.com>
2022-06-16 23:02:08 -0400

    Update 2) Multiple Of Three.cs

--- 2) Multiple Of Three.cs
+++ 2) Multiple Of Three.cs
@@ -1,6 +1,8 @@
-/*You are an elementary school teacher and explaining multiplication to students.
+/*
+You are an elementary school teacher and explaining multiplication to students.
 You are going to use multiplication by 3 as your example.
-The program you are given takes N number as input. Write a program to output all numbers from 1 to N, replacing all numbers that are multiples of 3 by "*".*/
+The program you are given takes N number as input. Write a program to output all numbers from 1 to N, replacing all numbers that are multiples of 3 by "*".
+*/
 
 using System;
 using System.Collections.Generic;
